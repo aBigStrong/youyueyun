82593d6cec40f5bf0d03020ef0e1fe69444f4477
Da Zhuang <617571982@qq.com>
2016-11-10 14:15:20 +0800

    1
    321

---
+++ lib/static/date/hello.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-Type: application/json');
+echo '{
+	"person1": {
+		"id": 1,
+		"name": "Jack",
+		"age": 30,
+		"sex": "Male",
+		"money": 100
+	},
+	"person2": {
+		"id": 2,
+		"name": "Jack",
+		"age": 30,
+		"sex": "Male",
+		"money": 100
+	},
+	"person3": {
+		"id": 3,
+		"name": "Jack",
+		"age": 30,
+		"sex": "Male",
+		"money": 100
+	},
+	"person4": {
+		"id": 4,
+		"name": "Jack",
+		"age": 30,
+		"sex": "Male",
+		"money": 100
+	}
+}';
+?>
